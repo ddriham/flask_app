b0ef228280069f3bdd3692951a76cea1c4d06c99
David Driham <david.driham@gmail.com>
2024-03-08 17:30:34 +0200

    adding the nginx container with the reverse proxy

--- flask/app.py
+++ flask/app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 docker_client = docker.from_env()
 
-@app.route('/')
+@app.route('/flask')
 def list_running_containers():
     containers = docker_client.containers.list()
     containers_names = [container.name for container in containers]
     return '<br>'.join(containers_names)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
